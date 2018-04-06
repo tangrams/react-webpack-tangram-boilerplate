679276b0664ded980b560ae61699354208b69642
Peter Richardson <github@zoomy.net>
2018-04-06 12:42:35 -0700

    use nextzen and react-leaflet

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,6 @@
 // -- Import dependencies --
-import L from 'leaflet';
 import React from 'react';
+import { Map } from 'react-leaflet';
 
 // -- Import Tangram --
 
@@ -25,34 +25,36 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
 
-// NOTE: This uses React to create a "wrapper" component around an HTML element
-// that Leaflet hooks into. This means you need to manage Leaflet's state
-// manually, which may not be what you want to work in a pure React way.
-// As an alternative, you may want to check out https://github.com/PaulLeCam/react-leaflet
+// NOTE: This uses react-leaflet to use Leaflet as a proper React component.
+// https://github.com/PaulLeCam/react-leaflet
 
-export default class Map extends React.Component {
+export default class LeafletMap extends React.Component {
   componentDidMount() {
-    const map = L.map(this.mapEl);
     const layer = Tangram.leafletLayer({
       scene: {
         import: [
-          'https://mapzen.com/carto/bubble-wrap-style/8/bubble-wrap-style.zip',
-          'https://mapzen.com/carto/bubble-wrap-style/8/themes/label-10.zip'
+          'https://www.nextzen.org/carto/bubble-wrap-style/8/bubble-wrap-style.zip',
+          'https://www.nextzen.org/carto/bubble-wrap-style/8/themes/label-10.zip'
         ],
-        // TODO: get your own API key at https://mapzen.com/dashboard/. It's free!
-        'global': { 'sdk_mapzen_api_key': 'mapzen-8SeUVxf' }
-      },
-      attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/">Mapzen</a>'
+        sources: {
+          mapzen: {
+            url: 'https://tile.nextzen.org/tilezen/vector/v1/256/all/{z}/{x}/{y}.mvt',
+            url_params: {
+                api_key: 'tsINU1vsQnKLU1jjCimtVw'
+            },
+            tile_size: 256,
+            max_zoom: 16
+          }
+        }
+      }
     });
 
-    layer.addTo(map);
-
-    map.setView([40.70531887544228, -74.00976419448853], 15);
+    layer.addTo(this.map.leafletElement);
   }
 
   render() {
     return (
-      <div ref={(ref) => {this.mapEl = ref }} />
+      <Map center={[40.70532, -74.00976]} zoom={15} ref={(ref) => { this.map = ref }} />
     );
   }
 }
